119cc601aa14bd45b0ca5afd8807763c68e56e8a
Mark Martene <markmartene@gmail.com>
2014-03-23 22:17:47 -0700

    Fixed chris's blunders

--- Assignment_2/samplefiles/sender.cpp
+++ Assignment_2/samplefiles/sender.cpp
@@ -8,7 +8,7 @@
 #include "msg.h"    /* For the message struct */
 
 /* The size of the shared memory chunk */
-#define SHARED_MEMORY_CHUNK_SIZE 1000
+#define SHARED_MEMORY_CHUNK_SIZE 1024
 
 /* The ids for the shared memory segment and the message queue */
 int shmid, msgid;
@@ -142,7 +142,7 @@ void send(const char* fileName)
 
 		if(msgsnd (msgid, &sndMsg.text, strlen(sndMsg.text)+1, IPC_NOWAIT) == -1)
 		{
-    		perror("Errror in send");
+    		perror("Error in send");
     		break;
     	}
     	else
