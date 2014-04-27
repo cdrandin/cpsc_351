5614d5c8b3f2383d4e7634e46b8ccaa34d9e99af
Christopher Randin <cdrandin@hotmail.com>
2014-04-26 20:04:12 -0700

    Start to parse through file.

--- Project3/program/main.cpp
+++ Project3/program/main.cpp
@@ -5,19 +5,21 @@
 
 const char* FILE_NAME = "in1.txt";
 
+inline const void MemFail();
+
 int main(int argc, char const *argv[])
 {	
 	int* page_sizes;
 	uint* memory_size;
 	char* file_name;
 	std::ifstream* infile;
+	int* num_process;
 
 	page_sizes = new int[3];
 
 	if(page_sizes == NULL)
 	{
-		puts("Error: memory could not be allocated");
-		exit(-1);
+		MemFail();
 	}
 
 	// Constant page numbers
@@ -27,6 +29,11 @@ int main(int argc, char const *argv[])
 
 	memory_size = new uint;
 	
+	if(memory_size == NULL)
+	{
+		MemFail();
+	}
+
     system("clear");
 
 	// Ask for memory size
@@ -35,6 +42,11 @@ int main(int argc, char const *argv[])
 
 	// Ask for page size. Keep asking till correct selection		
 	short* page_i = new short;
+	if(page_i == NULL)
+	{
+		MemFail();
+	}
+
 	for(int i=0;i==0;)
 	{
 		std::cout << "Page Size (1: 100, 2: 200, 3: 400)> ";
@@ -50,22 +62,50 @@ int main(int argc, char const *argv[])
 	}
 
 	file_name = new char[10];
+	if(file_name == NULL)
+	{
+		MemFail();
+	}
 
 	// Get workload file
 	std::cout << "Enter the file name: ";
-	std::cin >> *file_name;
+	std::cin.ignore();
+	std::cin.getline(file_name, 10);
 
 	infile = new std::ifstream(file_name);
 
-	infile->close();
+	if(infile == NULL)
+	{
+		MemFail();
+	}
 
+	if(infile->is_open())
+	{
+		num_process = new int;
+		*infile >> *num_process;
 
+		std::cout << *num_process << std::endl;
+		infile->close();
+
+		delete   num_process;
+
+	}
+	else
+	{
+		puts("Unable to open file");
+	}
 
 	delete[] page_sizes;
-	delete memory_size;
-	delete page_i;
+	delete   memory_size;
+	delete   page_i;
 	delete[] file_name;
-	delete infile;
+	delete   infile;
 
 	return 0;
+}
+
+inline const void MemFail()
+{
+	puts("Error: memory could not be allocated");
+	exit(-1);	
 }
